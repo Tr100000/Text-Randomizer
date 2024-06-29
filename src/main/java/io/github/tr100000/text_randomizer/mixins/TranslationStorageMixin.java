88cfcf9e5803ca7e7853b5fa71333d1d71c18884
Tr100000 <147051084+Tr100000@users.noreply.github.com>
2024-06-29 20:41:22 +0900

    Spotless

--- src/main/java/io/github/tr100000/text_randomizer/mixins/TranslationStorageMixin.java
+++ src/main/java/io/github/tr100000/text_randomizer/mixins/TranslationStorageMixin.java
@@ -16,45 +16,45 @@ import net.minecraft.client.resource.language.TranslationStorage;
 
 @Mixin(TranslationStorage.class)
 public class TranslationStorageMixin {
-	@ModifyVariable(method = "<init>", at = @At("HEAD"), ordinal = 0)
+    @ModifyVariable(method = "<init>", at = @At("HEAD"), ordinal = 0)
     private static Map<String, String> shuffle(Map<String, String> translations) {
         if (TextRandomizer.modEnabled) {
-			Map<String, String> shuffled = new HashMap<>();
+            Map<String, String> shuffled = new HashMap<>();
 
-			if (TextRandomizer.ignoreFormatSpecifiers) {
-				List<String> originalKeys = new ArrayList<>(translations.keySet());
-				List<String> shuffledKeys = new ArrayList<>(originalKeys);
-				Collections.shuffle(shuffledKeys);
-				
-				for (int i = 0; i < originalKeys.size(); i++) {
-					shuffled.put(originalKeys.get(i), translations.get(shuffledKeys.get(i)));
-				}
-			}
-			else {
-				Map<Integer, Map<String, String>> seperatedMap = new HashMap<>();
-				translations.entrySet().forEach(entry -> {
-					int count = entry.getValue().split("%(\\d\\$)?s", -1).length;
-					seperatedMap.computeIfAbsent(count, HashMap::new);
-					seperatedMap.get(count).put(entry.getKey(), entry.getValue());
-				});
-				
-				seperatedMap.forEach((count, map) -> {
-					List<String> originalKeys = new ArrayList<>(map.keySet());
-					List<String> shuffledKeys = new ArrayList<>(originalKeys);
-					Collections.shuffle(shuffledKeys);
-					
-					for (int i = 0; i < originalKeys.size(); i++) {
-						shuffled.put(originalKeys.get(i), map.get(shuffledKeys.get(i)));
-					}
-				});
-			}
-			
-			TextRandomizer.trySaveLanguage(shuffled);
-			return shuffled;
-		}
-		else {
-			TextRandomizer.trySaveLanguage(translations);
-			return translations;
-		}
+            if (TextRandomizer.ignoreFormatSpecifiers) {
+                List<String> originalKeys = new ArrayList<>(translations.keySet());
+                List<String> shuffledKeys = new ArrayList<>(originalKeys);
+                Collections.shuffle(shuffledKeys);
+
+                for (int i = 0; i < originalKeys.size(); i++) {
+                    shuffled.put(originalKeys.get(i), translations.get(shuffledKeys.get(i)));
+                }
+            }
+            else {
+                Map<Integer, Map<String, String>> seperatedMap = new HashMap<>();
+                translations.entrySet().forEach(entry -> {
+                    int count = entry.getValue().split("%(\\d\\$)?s", -1).length;
+                    seperatedMap.computeIfAbsent(count, HashMap::new);
+                    seperatedMap.get(count).put(entry.getKey(), entry.getValue());
+                });
+
+                seperatedMap.forEach((count, map) -> {
+                    List<String> originalKeys = new ArrayList<>(map.keySet());
+                    List<String> shuffledKeys = new ArrayList<>(originalKeys);
+                    Collections.shuffle(shuffledKeys);
+
+                    for (int i = 0; i < originalKeys.size(); i++) {
+                        shuffled.put(originalKeys.get(i), map.get(shuffledKeys.get(i)));
+                    }
+                });
+            }
+
+            TextRandomizer.trySaveLanguage(shuffled);
+            return shuffled;
+        }
+        else {
+            TextRandomizer.trySaveLanguage(translations);
+            return translations;
+        }
     }
 }
